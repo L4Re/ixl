7f0e2a5f1ae7386071742dbc4185e8d26b2a4280
Till Miemietz <till.miemietz@barkhauseninstitut.org>
2023-09-01 09:21:33 +0200

    Change ixy-pcap to print packets to stderr, adapt ned script.

--- server/ixy-pcap/ixy-pcap.cc
+++ server/ixy-pcap/ixy-pcap.cc
@@ -1,64 +1,39 @@
+/*****************************************************************************
+ *                                                                           *
+ *    ixy-pcap - Dump raw ethernet frames received on a NIC to stderr        *
+ *                                                                           *
+ * This version of ixy-pcap is an adapted form of the original ixy-pcap of   *
+ * the ixy driver package.                                                   *
+ *                                                                           *
+ *****************************************************************************/
+
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/time.h>
 
 #include <l4/ixylon/device.h>
 #include <l4/ixylon/memory.h>
+#include <l4/ixylon/log.h>
 
 using Ixl::Ixl_device;
 
 const int BATCH_SIZE = 32;
 
-// From https://wiki.wireshark.org/Development/LibpcapFileFormat
-typedef struct pcap_hdr_s {
-	uint32_t magic_number;  /* magic number */
-	uint16_t version_major; /* major version number */
-	uint16_t version_minor; /* minor version number */
-	int32_t  thiszone;      /* GMT to local correction */
-	uint32_t sigfigs;       /* accuracy of timestamps */
-	uint32_t snaplen;       /* max length of captured packets, in octets */
-	uint32_t network;       /* data link type */
-} pcap_hdr_t;
-
-typedef struct pcaprec_hdr_s {
-	int64_t ts_sec;         /* timestamp seconds */
-	int64_t ts_usec;        /* timestamp microseconds */
-	uint32_t incl_len;      /* number of octets of packet saved in file */
-	uint32_t orig_len;      /* actual length of packet */
-} pcaprec_hdr_t;
-
 int main(int argc, char* argv[]) {
-	if (argc < 3 || argc > 4) {
-		printf("Usage: %s <pci bus id> <output file> [n packets]\n", argv[0]);
+	if (argc < 2 || argc > 3) {
+		printf("Usage: %s <pci bus id> [n packets]\n", argv[0]);
 		return 1;
 	}
 
 	Ixl_device* dev = Ixl_device::ixl_init(argv[1], 1, 1, 0);
 
-	FILE* pcap = fopen(argv[2], "wb");
-	if (pcap == NULL) {
-		ixl_warn("failed to open file %s, falling back to stdout", argv[2]);
-        pcap = stdout;
-	}
-
 	int64_t n_packets = -1;
-	if (argc == 4) {
-		n_packets = atol(argv[3]);
+	if (argc == 3) {
+		n_packets = atol(argv[2]);
 		printf("Capturing %ld packets...\n", n_packets);
 	} else {
 		printf("Capturing packets...\n");
 	}
-
-	pcap_hdr_t header = {
-		.magic_number =  0xa1b2c3d4,
-		.version_major = 2,
-		.version_minor = 4,
-		.thiszone = 0,
-		.sigfigs = 0,
-		.snaplen = 65535,
-		.network = 1, // Ethernet
-	};
-	fwrite(&header, sizeof(header), 1, pcap);
 
 	struct Ixl::pkt_buf* bufs[BATCH_SIZE];
 	while (n_packets != 0) {
@@ -67,15 +42,13 @@ int main(int argc, char* argv[]) {
 		gettimeofday(&tv, NULL);
 
 		for (uint32_t i = 0; i < num_rx && n_packets != 0; i++) {
-			pcaprec_hdr_t rec_header = {
-				.ts_sec = tv.tv_sec,
-				.ts_usec = tv.tv_usec,
-				.incl_len = bufs[i]->size,
-				.orig_len = bufs[i]->size
-			};
-			fwrite(&rec_header, sizeof(pcaprec_hdr_t), 1, pcap);
 
-			fwrite(bufs[i]->data, bufs[i]->size, 1, pcap);
+            fprintf(stderr, "===== Packet Received at %06ld:%06ld "
+                    "(Payload length %u Bytes) =====\n", 
+                    tv.tv_sec, tv.tv_usec, bufs[i]->size);
+            hexdump(bufs[i]->data, bufs[i]->size);
+            fprintf(stderr, "=============================================="
+                            "=========================\n");
 
 			Ixl::pkt_buf_free(bufs[i]);
 			// n_packets == -1 indicates unbounded capture
@@ -85,6 +58,5 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
-	fclose(pcap);
 	return 0;
 }
